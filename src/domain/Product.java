291f84f8a1f81bcc2ad15fdaac0e0f513e6de8da
david <davdiaavi@gmail.com>
2019-09-19 16:21:09 +0200

    product

--- src/domain/Product.java
+++ src/domain/Product.java
@@ -1,4 +1,39 @@
 package domain;
 
 public class Product {
+    private String title;
+    private String type;
+    private String id;
+
+    public Product(String title, String type, String id){
+        setTitle(title);
+        setType(type);
+        setId(id);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
 }
